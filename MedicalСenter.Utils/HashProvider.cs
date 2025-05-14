62a1b35530e3870c3c3dcd6f6150c3ee42ec8f69
Kirill <kirill.safronov.00@mail.ru>
2025-05-14 23:43:39 +0300

    add

---
+++ MedicalСenter.Utils/HashProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MedicalСenter.Utils
+{
+    public static class HashProvider
+    {
+        public static string CreateSHA256(string input)
+        {
+            return Convert.ToHexString(SHA256.HashData(Encoding.ASCII.GetBytes(input)));
+        }
+    }
+}
